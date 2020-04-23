5c55fe301d3e28fdb3ce5187ec5c9af6a6149e2d
Chris Hurley <crhurley@gmail.com>
2020-04-23 15:44:57 +0100

    Add thread per client and handle joining or leaving a game

--- client.py
+++ client.py
@@ -25,16 +25,16 @@ class Client():
         host='127.0.0.1'
         port=1337
 
-        #Create socket object
+        #Create socket object to connect to server
         try:
-            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             logging.info("Client socket connected")
         except socket.error:
             logging.error("Could not create client socket")
 
         #Connect to specified port
         try:
-            self.client_socket.connect((host,port))
+            self.server_socket.connect((host,port))
             logging.info("Successfully connected to host %s at port %s", host, port)
         except socket.error:
             logging.error("Failed to connect to host %s at port %s", host, port)
@@ -43,20 +43,91 @@ class Client():
     def send_username(self):
         # Send username to server to register as a new player
         client_message = json.dumps({"new_player": self.username})
-        self.client_socket.send(client_message.encode())
+        self.server_socket.send(client_message.encode())
         logging.info("User %s sent to server", self.username)
 
         # Get response from server
-        from_server = self.client_socket.recv(4096)
-        print(from_server.decode())
+        from_server = self.server_socket.recv(4096).decode()
+        print(from_server)
+
+        # Exit if we already have two players
+        if "400 FULL" in from_server:
+            print("Sorry %s, we already have two players " % (self.username))
+            print("Exiting")
+            self.server_socket.close()
+            sys.exit()
+
+    def play_game(self):
+        #Initial response
+        response="WAIT"
+
+        """Client loop. Always either waiting for further instructions and not taking user input
+        or taking user input and returning server response"""
+        while True:
+
+            while "WAIT" in response:
+                response=json.loads(self.server_socket.recv(4096).decode())
+                logging.info("Response from server %s: ", response)
+
+
+                # Wait for second player
+                while response["status"] == "200 WAIT":
+                    response=json.loads(self.server_socket.recv(4096).decode())
+                    print("We are waiting for one more player")
+
+                while response["status"] == "200 READY":
+                    logging.info("Game started")
+                    #Take user input from command line interface
+                    user_input=input("5row->")
+                    if(user_input==''):
+                        continue
+
+                    #Send user input to server, and collect response
+                    client_message = json.dumps({"current_player": self.username, "next_move": user_input})
+                    self.server_socket.send(client_message.encode())
+                    response=json.loads(self.server_socket.recv(4096).decode())
+                    logging.info("Response from server %s: ", response)
+
+                    #Exit loop and quit program if user chooses to exit
+                    if response["status"] == "200 DISC":
+                        self.server_socket.close()
+                        sys.exit()
+
+                    #Error handling
+                    elif response["status"]=="400 ERR":
+                        print("Invalid command, please try again")
+                        response["status"]="200 READY"
+
+                #If response is valid, print it
+                else:
+                    print(response)
+
+    def exit(self):
+        try:
+            # Send exit message to server
+            client_message = json.dumps({"current_player": self.username, "next_move": "exit"})
+            self.server_socket.send(client_message.encode())
+
+            # Make sure to close the socket when we're done
+            self.server_socket.close()
+        except:
+            logging.info("Socket already closed")
 
     def run(self):
-        self.get_username()
-        self.setup_connection()
-        self.send_username()
+        try:
+            self.get_username()
+            self.setup_connection()
+            self.send_username()
+            self.play_game()
 
-        # Close the socket
-        self.client_socket.close()
+            self.exit()
+
+        # Handle KeyboardInterrupt
+        except KeyboardInterrupt:
+            logging.info("KeyboardInterrupt received, exiting")
+            self.exit()
+            sys.exit()
+
 
 if __name__ == "__main__":
     Client().run()
--- server.py
+++ server.py
@@ -3,6 +3,9 @@ import json
 import logging
 import os
 import sys
+import threading
+from threading import RLock
+import time
 
 class Server():
     # Set log level to environment variable LOGLEVEL
@@ -12,68 +15,128 @@ class Server():
     def __init__(self, players=[]):
         #Keep track of players currently in the game
         self.players = players
+        # Lock for threads
+        self.lock=RLock()
 
     def setup_connection(self):
         # Setup connection parameters
         host='127.0.0.1'
         port=1337
 
-        #Create server socket object
+        #Create server socket object to allow connection from client
         try:
-            self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             logging.info("Server socket connected")
         except socket.error:
             logging.error("Could not create server socket")
+            sys.exit()
 
         # Allow socket to be reused for testing
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         #Bind to specified port
         try:
-            self.server_socket.bind((host,port))
+            self.client_socket.bind((host,port))
             logging.info("Successfully binded to host %s at port %s", host, port)
         except socket.error:
             logging.error("Failed to bind to host %s at port %s", host, port)
             sys.exit()
 
-        self.server_socket.listen(5)
+        self.client_socket.listen(5)
         logging.info("Listening")
+
+    def connect_client(self, client_connection):
+        """Run for each client that connects to the server
+        Handles messages sent from the client and sends corresponding responses"""
+
+        # Main connection loop. Handles all messages from
+        while True:
+            try:
+                cmd = client_connection.recv(4096).decode()
+                logging.info("Received cmd: %s", cmd)
+
+                if not cmd:
+                    logging.info("Empty message received")
+                    break
+
+                cmd = json.loads(cmd)
+                client_response = ""
+
+                if "current_player" in cmd.keys():
+                    self.current_player = cmd["current_player"]
+
+                # Add a new player and set up match
+                if "new_player" in cmd.keys():
+                    self.current_player = cmd["new_player"]
+                    if self.add_player(cmd["new_player"]):
+                        client_response = "Welcome to the game %s!" % (self.current_player)
+                        client_connection.send(client_response.encode())
+                        while len(self.players) == 1:
+                            client_response = json.dumps({"status": "200 WAIT"})
+                            client_connection.send(client_response.encode())
+                            time.sleep(2)
+                        if len(self.players) == 2:
+                            client_response = json.dumps({"status": "200 READY"})
+                            client_connection.send(client_response.encode())
+                    else:
+                        client_response = json.dumps({"status": "400 FULL"})
+                        client_connection.send(client_response.encode())
+
+                elif "next_move" in cmd.keys():
+                    move = cmd["next_move"]
+                    logging.info("Processing move %s: ", move)
+
+                    # Valid move
+                    if move.isdigit() and len(move) == 1:
+                        logging.info("Valid move %s: ", move)
+                        client_response = json.dumps({"status": "200 READY"})
+                        client_connection.send(client_response.encode())
+
+                    # User wants to exit
+                    elif move == "exit":
+                        logging.info("self.current_player, self.players: %s %s", self.current_player, self.players)
+                        self.lock.acquire()
+                        self.players.remove(self.current_player)
+                        client_response = json.dumps({"status": "200 DISC"})
+                        client_connection.send(client_response.encode())
+                        client_connection.close()
+                        self.lock.release()
+                        logging.info("Client %s disconnected", self.current_player)
+                        break
+
+                    # Unknown command
+                    else:
+                        client_response = json.dumps({"status": "400 ERR"})
+                        client_connection.send(client_response.encode())
+
+            except BrokenPipeError as bpe:
+                logging.info("Connection error from %s, removing them", self.current_player)
+                self.players.remove(self.current_player)
+
+
 
     # Add player to the game if there is space
     def add_player(self, player):
-        logging.info("Current players: %s", self.players)
         if len(self.players) < 2:
+            self.lock.acquire()
             self.players.append(player)
             logging.info("New player %s added", player)
-            response = "Welcome to the game " + player
-            return response
+            logging.info("Current players: %s", self.players)
+            self.lock.release()
+            return True
         else:
-            response = "Sorry %s, we already have two players " % (player)
-            return response
+            return False
+
 
 
     def run(self):
         self.setup_connection()
 
-        # Main connection loop. Handles all messages from client
+        # Main program loop
         while True:
-            conn, addr = self.server_socket.accept()
-            while True:
-                data = conn.recv(4096).decode()
-                logging.info("Received data: %s", data)
-
-                if not data:
-                    logging.info("Empty message received")
-                    break
-
-                data = json.loads(data)
-                # Add a new player
-                if "new_player" in data.keys():
-                    server_response = self.add_player(data["new_player"])
-                    conn.send(server_response.encode())
-
-            conn.close()
-            print('client disconnected')
+            conn, addr = self.client_socket.accept()
+            client_thread = threading.Thread(target=self.connect_client, args=(conn,))
+            client_thread.start()
 
 if __name__ == "__main__":
     Server().run()
