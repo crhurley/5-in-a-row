d58100ce595f757f1730c73ec4db65270176476d
Chris Hurley <crhurley@gmail.com>
2020-04-26 19:21:42 +0100

    Complete game logic and reformat

--- client.py
+++ client.py
@@ -27,7 +27,10 @@ class Client():
 
         #Create socket object to connect to server
         try:
-            self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server_socket = socket.socket(
+                socket.AF_INET,
+                socket.SOCK_STREAM
+            )
             logging.info("Client socket connected")
         except socket.error:
             logging.error("Could not create client socket")
@@ -35,39 +38,64 @@ class Client():
         #Connect to specified port
         try:
             self.server_socket.connect((host,port))
-            logging.info("Successfully connected to host %s at port %s", host, port)
+            logging.info(
+                "Successfully connected to host %s at port %s",
+                host,
+                port
+            )
         except socket.error:
-            logging.error("Failed to connect to host %s at port %s", host, port)
+            logging.error(
+                "Failed to connect to host %s at port %s",
+                host,
+                port
+            )
             sys.exit()
 
     def send_username(self):
         # Send username to server to register as a new player
-        client_message = json.dumps({"new_player": self.username})
+        client_message = json.dumps({
+            "new_player": self.username
+        })
         self.server_socket.send(client_message.encode())
         logging.info("User %s sent to server", self.username)
 
         # Get response from server
-        from_server = self.server_socket.recv(4096).decode()
-        print(from_server)
+        user_response =json.loads(
+            self.server_socket.recv(4096).decode()
+        )
+        logging.info("user_response %s ", user_response)
+
+        # Print message on successful join
+        if user_response["status"] == "200 JOIN":
+            print(f"Welcome to the game {self.username}")
 
         # Exit if we already have two players
-        if from_server == "400 FULL":
+        elif user_response["status"] == "400 FULL":
             print("Sorry %s, we already have two players " % (self.username))
             print("Exiting")
             self.server_socket.close()
             sys.exit()
 
     def play_game(self):
-        """Client loop. Always either waiting for further instructions and not taking user input
-        or taking user input and returning server response"""
+        """Client loop. Always either waiting for further
+        instructions and not taking user input or taking
+        user input and returning server response"""
         while True:
             self.response=json.loads(self.server_socket.recv(4096).decode())
-            logging.info("Start of client loop. Response from server %s: ", self.response)
+            logging.info(
+                "Start of client loop. Response from server %s: ",
+                self.response
+            )
+
+            # Make sure we don't prompt if the game is over
+            self.check_game_over(self.response)
 
             # Wait for second player to join
             while self.response["status"] == "200 WAIT_PLAYER":
                 print("We are waiting for one more player")
-                self.response=json.loads(self.server_socket.recv(4096).decode())
+                self.response=json.loads(
+                    self.server_socket.recv(4096).decode()
+                )
 
             # Wait for your first turn
             if self.response["status"] == "200 WAIT_TURN":
@@ -75,25 +103,39 @@ class Client():
 
             if self.response["status"] == "200 READY":
                 logging.info("Taking turn")
-                self.make_move()
+                if ("board" in self.response.keys() and
+                        "marker" in self.response.keys()):
+                    board = self.response["board"]
+                    marker = self.response["marker"]
+                    self.make_move(board, marker)
 
                 # Wait for your turn
                 if self.response["status"] == "200 WAIT_TURN":
                     print("Please wait for your opponent's turn")
 
-    def make_move(self):
+    def make_move(self, board, marker):
         input_validated = False
+        self.print_board(board, marker)
         # Keep prompting until the server accepts the input
         while not input_validated:
             #Take user input from command line interface
             user_input=input("5row->")
 
             #Send user input to server, and collect response
-            client_message = json.dumps({"current_player": self.username, "next_move": user_input})
+            client_message = json.dumps({
+                "current_player": self.username,
+                "next_move": user_input
+            })
             self.server_socket.send(client_message.encode())
-            self.response=json.loads(self.server_socket.recv(4096).decode())
-            logging.info("After move, response from server %s: ", self.response)
+            self.response=json.loads(
+                self.server_socket.recv(4096).decode()
+            )
+            logging.info(
+                "After move, response from server %s: ",
+                self.response
+            )
 
+            self.check_game_over(self.response)
             # Close socket and quit if user chooses to exit
             if self.response["status"] == "200 DISC":
                 self.server_socket.close()
@@ -101,15 +143,51 @@ class Client():
 
             #Error handling
             if self.response["status"]=="400 ERR":
-                print("Invalid command, please try again")
+                print(f"Sorry {self.username}, that's an invalid command. Please try again")
+                input_validated = False
+            elif self.response["status"]=="400 COL_FULL":
+                print(f"Sorry {self.username}, that column is full")
                 input_validated = False
             else:
                 input_validated = True
 
+    def print_board(self, board, marker):
+        print("The current state of the board is:")
+        boardwidth = len(board[0])
+        for row in board:
+            print(" ".join(row))
+        print(
+            f"Please enter a number between 1-{boardwidth} to place an {marker}"
+        )
+
+    def check_game_over(self, response):
+        # Close socket and quit if server says to disconnect
+        if response["status"] == "200 DISC":
+            print("Server error. Have to disconnect")
+            self.exit()
+
+        # Close socket and quit if user won
+        if response["status"] == "200 WIN":
+            print(f"Congratulations {self.username}! You won!")
+            self.exit()
+
+        # Close socket and quit if user lost
+        if response["status"] == "200 LOSS":
+            print(f"Sorry {self.username}. You lost!")
+            self.exit()
+
+        # Close socket and quit if draw
+        if response["status"] == "200 DRAW":
+            print(f"The game ended in a draw {self.username}")
+            self.exit()
+
     def exit(self):
         try:
             # Send exit message to server
-            client_message = json.dumps({"current_player": self.username, "next_move": "exit"})
+            client_message = json.dumps({
+                "current_player": self.username,
+                "next_move": "exit"
+            })
             self.server_socket.send(client_message.encode())
 
             # Make sure to close the socket when we're done
--- server.py
+++ server.py
@@ -13,18 +13,39 @@ class Server():
     logging.basicConfig(level=os.environ.get("LOGLEVEL", "ERROR"))
 
     def __init__(self, players=[]):
+        self.reset_server(players)
+
+    def reset_server(self, players=[]):
         # Keep track of players currently in the game
         self.players = players
         # Keep track of connections
         self.connections = {}
+        # Lock for threads
+        self.lock=RLock()
         # Keep track of who's turn it is
         self.turn = ""
         self.waiting = ""
+        self.markers = {}
         # Keep track of whether we've started the game
         # So that we don't try start it multiple times
         self.game_started = False
-        # Lock for threads
-        self.lock=RLock()
+        # Board size
+        self.boardheight = 5
+        self.boardwidth = 5
+        # Board state
+        self.board =[]
+        # Check if column is full
+        self.column_full = False
+
+        # Don't start the server unless the board is the right size
+        if not(5 <= self.boardwidth <= 9 and 5 <= self.boardheight <= 9):
+            logging.error(
+                "SERVER ERROR: Board height and width \
+                must be between 5 and 9"
+            )
+            sys.exit()
+
+
 
     def setup_connection(self):
         # Setup connection parameters
@@ -33,21 +54,36 @@ class Server():
 
         #Create server socket object to allow connection from client
         try:
-            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket = socket.socket(
+                socket.AF_INET,
+                socket.SOCK_STREAM
+            )
             logging.info("Server socket connected")
         except socket.error:
             logging.error("Could not create server socket")
             sys.exit()
 
         # Allow socket to be reused for testing
-        self.client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.client_socket.setsockopt(
+            socket.SOL_SOCKET,
+            socket.SO_REUSEADDR,
+            1
+        )
 
         #Bind to specified port
         try:
             self.client_socket.bind((host,port))
-            logging.info("Successfully binded to host %s at port %s", host, port)
+            logging.info(
+                "Successfully binded to host %s at port %s",
+                host,
+                port
+            )
         except socket.error:
-            logging.error("Failed to bind to host %s at port %s", host, port)
+            logging.error(
+                "Failed to bind to host %s at port %s",
+                host,
+                port
+            )
             sys.exit()
 
         self.client_socket.listen(5)
@@ -55,7 +91,8 @@ class Server():
 
     def connect_client(self, client_connection):
         """Run for each client that connects to the server
-        Handles messages sent from the client and sends corresponding responses"""
+        Handles messages sent from the client and sends
+        corresponding responses"""
 
         # Main connection loop. Handles all messages from client
         while True:
@@ -78,7 +115,9 @@ class Server():
                 if "new_player" in cmd.keys():
                     self.current_player = cmd["new_player"]
                     if self.add_player(cmd["new_player"]):
-                        client_response = "Welcome to the game %s!" % (self.current_player)
+                        client_response = json.dumps({
+                            "status": "200 JOIN"
+                        })
                         client_connection.send(client_response.encode())
 
                         # Add new player's connection to the list
@@ -88,20 +127,30 @@ class Server():
 
                         # Waiting for second player
                         while len(self.players) == 1:
-                            client_response = json.dumps({"status": "200 WAIT_PLAYER"})
+                            client_response = json.dumps({
+                                "status": "200 WAIT_PLAYER"
+                            })
                             client_connection.send(client_response.encode())
                             time.sleep(2)
 
                         # Second player has arrived we can start the game
                         if len(self.players) == 2 and not self.game_started:
-                            # The first player to connect gets the first turn
+                            # Initialise turn and markers
                             self.turn = self.players[0]
+                            self.markers[self.players[0]] = 'X'
                             self.waiting = self.players[1]
-                            logging.info("Starting game: %s goes first: ", self.turn)
+                            self.markers[self.players[1]] = 'O'
+                            logging.info(
+                                "Starting game: %s goes first: ",
+                                self.turn
+                            )
+                            self.generate_board()
                             self.prompt_players()
                             self.game_started = True
                     else:
-                        client_response = json.dumps({"status": "400 FULL"})
+                        client_response = json.dumps({
+                            "status": "400 FULL"
+                        })
                         client_connection.send(client_response.encode())
 
                 elif "next_move" in cmd.keys():
@@ -109,20 +158,36 @@ class Server():
                     logging.info("Processing move %s: ", move)
 
                     # Valid move
-                    if move.isdigit() and len(move) == 1:
+                    if (move.isdigit() and len(move) == 1
+                            and int(move) <= self.boardwidth):
                         logging.info("Valid move %s: ", move)
-                        self.prompt_players()
+                        self.mark_board(int(move))
+
+                        # Don't prompt both players if the
+                        # last column chosen was full
+                        if not self.column_full:
+                            self.prompt_players()
+                        else:
+                            self.column_full = False
 
                     # User wants to exit
                     elif move == "exit":
-                        logging.info("self.current_player, self.players: %s %s", self.current_player, self.players)
-                        self.lock.acquire()
-                        self.players.remove(self.current_player)
-                        self.lock.release()
-                        client_response = json.dumps({"status": "200 DISC"})
+                        logging.info(
+                            "self.current_player, self.players: %s %s",
+                            self.current_player,
+                            self.players
+                        )
+                        if self.current_player in self.players:
+                            self.lock.acquire()
+                            self.players.remove(self.current_player)
+                            self.lock.release()
+                        client_response = json.dumps({
+                            "status": "200 DISC"
+                        })
                         client_connection.send(client_response.encode())
                         client_connection.close()
-                        logging.info("Client %s disconnected", self.current_player)
+                        logging.info("Client disconnected")
+                        self.disconnect_clients()
                         break
 
                     # Unknown command
@@ -130,11 +195,9 @@ class Server():
                         client_response = json.dumps({"status": "400 ERR"})
                         client_connection.send(client_response.encode())
 
-            except BrokenPipeError as bpe:
-                logging.info("Connection error from %s, removing them", self.current_player)
-                self.players.remove(self.current_player)
-
-
+            except (BrokenPipeError, ConnectionResetError) as conn_err:
+                logging.error("Connection error")
+                self.disconnect_clients()
 
     # Add player to the game if there is space
     def add_player(self, player):
@@ -150,13 +213,6 @@ class Server():
 
     def prompt_players(self):
         """Prompts players for move and automatically switches turn"""
-        logging.info("It's: %s's turn. %s is waiting", self.turn, self.waiting)
-
-        # Tell each player it's their go or else to wait
-        ready_response = json.dumps({"status": "200 READY", "turn": self.turn})
-        self.connections[self.turn].send(ready_response.encode())
-        wait_response = json.dumps({"status": "200 WAIT_TURN"})
-        self.connections[self.waiting].send(wait_response.encode())
 
         # Switch turn
         if self.turn==self.players[0]:
@@ -165,6 +221,136 @@ class Server():
         else:
             self.turn=self.players[0]
             self.waiting=self.players[1]
+        logging.info(
+            "It's: %s's turn. %s is waiting",
+            self.turn,
+            self.waiting
+        )
+
+        # Tell each player whether to go or else to wait
+        ready_json = {
+            "status": "200 READY",
+            "board": self.board,
+            "marker": self.markers[self.turn]
+        }
+        ready_response = json.dumps(ready_json)
+        self.connections[self.turn].send(ready_response.encode())
+        wait_json = {
+            "status": "200 WAIT_TURN",
+            "board": self.board,
+            "marker": self.markers[self.waiting]
+        }
+        wait_response = json.dumps(wait_json)
+        self.connections[self.waiting].send(wait_response.encode())
+
+
+    #generate empty board
+    def generate_board(self):
+        # Maintain board as a list of rows
+        for i in range(self.boardheight):
+            self.board.append(["[ ]"] * self.boardwidth)
+
+    def mark_board(self, move):
+        # Decrement move as list starts at 0
+        move -= 1
+        board_marked = False
+        # [0,0] is top left of the board so we need to loop
+        # from bottom to top of the column represented by move
+        # Offset by -1 as list starts at 0
+        for i in range(self.boardheight -1 , -1, -1):
+            if self.board[i][move] == "[ ]" and not board_marked:
+                self.board[i][move] = f"[{self.markers[self.turn]}]"
+                board_marked = True
+
+        # If we get to the end without marking
+        # the column must be full
+        if not board_marked:
+            self.column_full = True
+            col_full_response = json.dumps({"status": "400 COL_FULL"})
+            self.connections[self.turn].send(col_full_response.encode())
+            logging.info("Column full")
+
+        if self.check_winner():
+            # Inform players of result
+            logging.info("%s has won!", self.turn)
+            win_response = json.dumps({"status": "200 WIN"})
+            self.connections[self.turn].send(win_response.encode())
+            loss_response = json.dumps({"status": "200 LOSS"})
+            self.connections[self.waiting].send(loss_response.encode())
+            # TODO exit gracefully
+            # sys.exit()
+
+        if self.check_draw():
+            # Inform players of result
+            logging.info("The game has ended in a draw")
+            draw_response = json.dumps({"status": "200 DRAW"})
+            self.connections[self.turn].send(draw_response.encode())
+            self.connections[self.waiting].send(draw_response.encode())
+            # TODO exit gracefully
+            # sys.exit()
+
+    def check_winner(self):
+        # x is the row number y is the column number
+
+        #check horizontal spaces
+        for y in range(self.boardwidth):
+            for x in range(self.boardheight - 4):
+                if (self.markers[self.turn] in self.board[x][y] and
+                        self.markers[self.turn] in self.board[x+1][y] and
+                        self.markers[self.turn] in self.board[x+2][y] and
+                        self.markers[self.turn] in self.board[x+3][y] and
+                        self.markers[self.turn] in self.board[x+4][y]):
+                    return True
+
+        #check vertical spaces
+        for x in range(self.boardheight):
+            for y in range(self.boardwidth - 4):
+                if (self.markers[self.turn] in self.board[x][y] and
+                        self.markers[self.turn] in self.board[x][y+1] and
+                        self.markers[self.turn] in self.board[x][y+2] and
+                        self.markers[self.turn] in self.board[x][y+3] and
+                        self.markers[self.turn] in self.board[x][y+4]):
+                    return True
+
+        # #check / diagonal spaces
+        for x in range(self.boardheight - 4):
+            for y in range(4, self.boardwidth):
+                if (self.markers[self.turn] in self.board[x][y] and
+                        self.markers[self.turn] in self.board[x+1][y-1] and
+                        self.markers[self.turn] in self.board[x+2][y-2] and
+                        self.markers[self.turn] in self.board[x+3][y-3] and
+                        self.markers[self.turn] in self.board[x+4][y-4]):
+                    return True
+
+        #check \ diagonal spaces
+        for x in range(self.boardheight - 4):
+            for y in range(self.boardwidth - 4):
+                if (self.markers[self.turn] in self.board[x][y] and
+                        self.markers[self.turn] in self.board[x+1][y+1] and
+                        self.markers[self.turn] in self.board[x+2][y+2] and
+                        self.markers[self.turn] in self.board[x+3][y+3] and
+                        self.markers[self.turn] in self.board[x+4][y+4]):
+                    return True
+
+        return False
+
+    def check_draw(self):
+        # Check if the top row is full
+        if "[ ]" not in self.board[0]:
+            if not self.check_winner():
+                return True
+
+    def disconnect_clients(self):
+        logging.info("Disconnecting all players")
+        for player in self.players:
+            # Try tell each player to disconnect and remove them from list
+            try:
+                self.players.remove(player)
+                ready_response = json.dumps({"status": "200 DISC"})
+                self.connections[player].send(ready_response.encode())
+            except:
+                logging.info("Socket for %s already closed", player)
+        self.reset_server()
 
     def run(self):
         self.setup_connection()
@@ -172,7 +358,10 @@ class Server():
         # Main program loop
         while True:
             conn, addr = self.client_socket.accept()
-            client_thread = threading.Thread(target=self.connect_client, args=(conn,))
+            client_thread = threading.Thread(
+                target=self.connect_client,
+                args=(conn,)
+            )
             client_thread.start()
 
 if __name__ == "__main__":
